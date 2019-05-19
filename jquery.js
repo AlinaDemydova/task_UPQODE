6668667d330d3485ec534a90521775d6ab4e97e3
AlinaDemydova <web1demydova@gmail.com>
2019-05-19 12:02:41 +0300

    Add project

---
+++ jquery.js
@@ -0,0 +1,40 @@
+	$(document).ready(function() {
+		(function() { $('option:first').css('display', 'none'); })();
+		$('.nav-link').click(function(e) {
+			$('.nav-link').removeClass('activeMenu');
+			$(this).addClass('activeMenu');
+		})
+		$('.carousel').carousel({
+			interval: 100000
+		});
+		$('#form').validator();
+		$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+			if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass(
+					"show");
+			}
+			var $subMenu = $(this).next(".dropdown-menu");
+			$subMenu.toggleClass('show');
+			$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown',
+				function(e) {
+					$('.dropdown-submenu .show').removeClass("show");
+				});
+			return false;
+		});
+	});
+
+	function activeProgressbar() {
+		for (let i = 0; i < $('.progress-bar').length; i++) {
+			let progressWidth = (($('.progressbar').width()) * (parseInt($('h6>span:eq(' +
+				i + ')').html()))) / 100;
+			$('.progress-bar:eq(' + i + ')').animate({ width: '+=' + progressWidth },
+				1000);
+		}
+		$('h6>span').delay(1000).animate({ opacity: '+=1' }, 1000);
+		$('#progressbarContainer').removeAttr('onmouseover');
+	}
+
+	function adventagesHide(toHide, toVisible) {
+		$('#' + toHide).css('display', 'none');
+		$('#' + toVisible).css('display', 'block');
+	}
